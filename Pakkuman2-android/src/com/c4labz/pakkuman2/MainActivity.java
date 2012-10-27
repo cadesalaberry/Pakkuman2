d00319eb29c581a7f01ff118253bbcf6b893f16e
C-A de Salaberry <cadesalaberry@yahoo.com>
2012-10-27 15:44:02 -0400

    v0.0 - Starting the libgdx project.

---
+++ Pakkuman2-android/src/com/c4labz/pakkuman2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.c4labz.pakkuman2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new GDXPakkuman2(), cfg);
+    }
+}
---
+++ Pakkuman2-html/src/com/c4labz/pakkuman2/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.c4labz.pakkuman2.client;
+
+import com.c4labz.pakkuman2.GDXPakkuman2;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GDXPakkuman2();
+	}
+}
